10e03e9cd6213304f8bce2deef627489d37fa965
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-08-03 14:24:49 +0200

    szürés

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -28,19 +28,30 @@ class Student extends Model
         return $this->belongsTo(Section::class, 'section_id');
     }
 
+
+    /**
+     * Scope a query to search students by name or email, class, and section.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeSearch(Builder $query, Request $request)
     {
-        return $query->where(function ($query) use ($request) {
-            return $query->when($request->search, function ($query) use ($request) {
-                return $query->where(function ($query) use ($request) {
-                    $query->where('name', 'like', '%' . $request->search . '%')
-                        ->orWhere('email', 'like', '%' . $request->search . '%');
-                });
-            })->when($request->class_id, function ($query) use ($request) {
-                return $query->where('class_id', $request->class_id);
-            })->when($request->section_id, function($query) use($request) {
-                return $query->where('section_id', $request->section_id);
+        // If search parameter is present, filter results by name or email containing the search term
+        return $query->when($request->search, function ($query) use ($request) {
+            $query->where(function ($query) use ($request) {
+                $query->where('name', 'like', "%{$request->search}%")
+                    ->orWhere('email', 'like',  "%{$request->search}%");
             });
+        })
+        // If class_id parameter is present, filter results by that class_id
+        ->when($request->class_id, function ($query) use ($request) {
+            $query->where('class_id', $request->class_id);
+        })
+        // If section_id parameter is present, filter results by that section_id
+        ->when($request->section_id, function ($query) use ($request) {
+            $query->where('section_id', $request->section_id);
         });
     }
 }
